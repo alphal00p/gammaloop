b4045ef81630330cd2a252c600f312899a145daa
Mathijs Fraaije <mathijsfraaije@gmail.com>
2024-02-21 16:14:25 +0100

    Tests of full integration pipeline

--- python/gammaloop/tests/common.py
+++ python/gammaloop/tests/common.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from subprocess import Popen, PIPE
 import gammaloop
 from gammaloop.misc.common import GL_PATH, logger
+import shutil
+import yaml
+
 pjoin = os.path.join
 
 RESOURCES_PATH = os.path.join(os.path.dirname(
@@ -45,3 +48,62 @@ def run_drawing(drawing_path: str) -> bool:
     logger.debug("DRAWING TEST STDOUT:\n%s", output.decode("utf-8"))
     logger.debug("DRAWING TEST STDERR:\n%s", error.decode("utf-8"))
     return True
+
+
+def scalar_euclidean_integration_test(graph: str, imag_phase: bool, target: float):
+    max_mc_error_dif = 5.0
+    max_rel_error_dif = 0.01
+    max_percent_error = 0.01
+
+    workspace = os.path.join(RESOURCES_PATH, 'test_{}'.format(graph))
+    graph_path = os.path.join(
+        RESOURCES_PATH, 'qgraf_outputs', '{}.py'.format(graph))
+    test_config_path = os.path.join(
+        RESOURCES_PATH, 'test_configs', '{}.yaml'.format(graph))
+
+    # clean up output if it is there
+    if os.path.exists(workspace):
+        shutil.rmtree(workspace)
+
+    gl = get_gamma_loop_interpreter()
+
+#     # create the output needed for the test
+    command_list = gammaloop.CommandList.from_string(
+        "import_model scalars-full")
+    command_list.add_command(
+        f"import_graphs {graph_path} --format=qgraph")
+    command_list.add_command(f"output {workspace}")
+
+    gl.run(command_list)
+
+    # copy settings
+    shutil.copyfile(test_config_path,
+                    os.path.join(workspace, 'cards', 'run_card.yaml'))
+
+    command_list = gammaloop.CommandList.from_string(
+        "launch {}".format(workspace))
+    command_list.add_command("integrate {}".format(graph))
+
+    gl.run(command_list)
+
+    f = open(os.path.join(workspace, 'runs', 'run.yaml'), "r").read()
+    run_yaml = yaml.safe_load(f)
+
+    res_for_check = run_yaml["result"][0]
+    error_for_check = run_yaml["error"][0]
+    zero_res = run_yaml["result"][1]
+    if imag_phase:
+        res_for_check = run_yaml["result"][1]
+        error_for_check = run_yaml["error"][1]
+        zero_res = run_yaml["result"][0]
+
+    assert zero_res == 0.0
+
+    absolute_difference = abs(res_for_check - target)
+    relative_difference = absolute_difference / abs(target)
+
+    assert absolute_difference < max_mc_error_dif * error_for_check
+    assert relative_difference < max_rel_error_dif
+    assert error_for_check < max_percent_error * abs(target)
+
+    shutil.rmtree(workspace)
--- python/gammaloop/tests/integration/test_integral.py
+++ python/gammaloop/tests/integration/test_integral.py
@@ -1,6 +1,9 @@
 # pylint: disable=unused-import
-from gammaloop.tests.common import get_gamma_loop_interpreter
-from gammaloop.interface.gammaloop_interface import CommandList
+from gammaloop.tests.common import scalar_euclidean_integration_test
+
+max_mc_error_dif = 5.0
+max_rel_error_dif = 0.01
+max_percent_error = 0.01
 
 
 class TestScalarTopologies:
@@ -10,3 +13,13 @@ class TestScalarTopologies:
         # TODO
         # gl = get_gamma_loop_interpreter()
         # gl.run(CommandList.from_string("import_model sm"))
+
+    def test_scalar_triangle(self):
+
+        scalar_euclidean_integration_test(
+            "massless_triangle", True, 0.00009765455799148221)
+
+    def test_tree_triangle(self):
+
+        scalar_euclidean_integration_test(
+            "tree_triangle", True, 0.00009765455799148221 / -49)
