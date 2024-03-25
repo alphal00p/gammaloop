d51cb5857a19e5b373be7ede785e8b411ddf3597
Mathijs Fraaije <mathijsfraaije@gmail.com>
2024-03-25 14:16:43 +0100

    introduction of IntegrationState struct

--- src/integrate.rs
+++ src/integrate.rs
@@ -32,6 +32,8 @@ use std::str::FromStr;
 use std::time::Instant;
 use tabled::{Style, Table, Tabled};
 
+const N_INTEGRAND_ACCUMULATORS: usize = 2;
+
 /// Intended to be a copy of the integrand for each core.
 pub struct UserData {
     pub integrand: Vec<Integrand>,
@@ -54,6 +56,41 @@ pub struct IntegralResult {
     pdf: String,
 }
 
+struct IntegrationState {
+    num_points: usize,
+    integral: StatisticsAccumulator<f64>,
+    all_integrals: Vec<StatisticsAccumulator<f64>>,
+    stats: StatisticsCounter,
+    rng: MonteCarloRng,
+    grid: Grid<f64>,
+    iter: usize,
+}
+
+impl IntegrationState {
+    fn new_from_settings<GridGenerator>(settings: &Settings, create_grid: GridGenerator) -> Self
+    where
+        GridGenerator: Fn() -> Grid<f64>,
+    {
+        let num_points = 0;
+        let iter = 0;
+        let rng = MonteCarloRng::new(settings.integrator.seed, 0);
+        let grid = create_grid();
+        let integral = StatisticsAccumulator::new();
+        let all_integrals = vec![StatisticsAccumulator::new(); N_INTEGRAND_ACCUMULATORS];
+        let stats = StatisticsCounter::new_empty();
+
+        Self {
+            num_points,
+            integral,
+            all_integrals,
+            stats,
+            rng,
+            grid,
+            iter,
+        }
+    }
+}
+
 /// Integrate function used for local runs
 pub fn havana_integrate<F>(
     settings: &Settings,
@@ -63,27 +100,17 @@ pub fn havana_integrate<F>(
 where
     F: Fn(&Settings) -> UserData,
 {
-    let mut num_points = 0;
-    const N_INTEGRAND_ACCUMULATORS: usize = 2;
+    let mut user_data = user_data_generator(settings);
+    let mut integration_state =
+        IntegrationState::new_from_settings(settings, || user_data.integrand[0].create_grid());
 
     let mut samples = vec![Sample::new(); settings.integrator.n_start];
     let mut f_evals = vec![vec![0.; N_INTEGRAND_ACCUMULATORS]; settings.integrator.n_start];
-    let mut integral: StatisticsAccumulator<f64> = StatisticsAccumulator::new();
-    let mut all_integrals = vec![StatisticsAccumulator::new(); N_INTEGRAND_ACCUMULATORS];
     let mut evaluation_results = vec![EvaluationResult::zero(); settings.integrator.n_start];
-    let mut stats = StatisticsCounter::new_empty();
-
-    let seed = settings.integrator.seed;
-    let thread_id = 0; // Samples are generated on a single core in this function.
-    let mut rng = MonteCarloRng::new(seed, thread_id);
-
-    let mut user_data = user_data_generator(settings);
-
-    let mut grid = user_data.integrand[0].create_grid();
 
     let grid_str = match &settings.sampling {
         SamplingSettings::MultiChanneling(_multi_channeling_settings) => {
-            let cont_dimension = match &grid {
+            let cont_dimension = match &integration_state.grid {
                 Grid::Continuous(g) => g.continuous_dimensions.len(),
                 _ => unreachable!(),
             };
@@ -95,7 +122,7 @@ where
             )
         }
         SamplingSettings::Default => {
-            let cont_dimension = match &grid {
+            let cont_dimension = match &integration_state.grid {
                 Grid::Continuous(g) => g.continuous_dimensions.len(),
                 _ => unreachable!(),
             };
@@ -103,7 +130,7 @@ where
             format!("a continuous {}-dimensional grid", cont_dimension)
         }
         SamplingSettings::DiscreteGraphs(discrete_graph_sampling_settings) => {
-            let num_graphs = match &grid {
+            let num_graphs = match &integration_state.grid {
                 Grid::Discrete(g) => g.bins.len(),
                 _ => unreachable!(),
             };
@@ -128,31 +155,33 @@ where
         }
     };
 
-    let mut iter = 0;
-
     let cores = user_data.integrand.len();
 
     let t_start = Instant::now();
 
     info!("Integrating over a {} ...\n", grid_str);
 
-    while num_points < settings.integrator.n_max {
-        let cur_points = settings.integrator.n_start + settings.integrator.n_increase * iter;
+    while integration_state.num_points < settings.integrator.n_max {
+        let cur_points =
+            settings.integrator.n_start + settings.integrator.n_increase * integration_state.iter;
         samples.resize(cur_points, Sample::new());
         f_evals.resize(cur_points, vec![0.; N_INTEGRAND_ACCUMULATORS]);
         evaluation_results.resize(cur_points, EvaluationResult::zero());
 
         for sample in &mut samples[..cur_points] {
-            grid.sample(&mut rng, sample);
+            integration_state
+                .grid
+                .sample(&mut integration_state.rng, sample);
         }
 
         // the number of points per core for all cores but the last, which may have fewer
         let nvec_per_core = (cur_points - 1) / cores + 1;
 
-        let current_max_eval = integral
+        let current_max_eval = integration_state
+            .integral
             .max_eval_positive
             .abs()
-            .max(integral.max_eval_negative.abs());
+            .max(integration_state.integral.max_eval_negative.abs());
 
         user_data.integrand[..cores]
             .par_iter_mut()
@@ -166,7 +195,7 @@ where
                     let fc = integrand_f.evaluate_sample(
                         s,
                         s.get_weight(),
-                        iter,
+                        integration_state.iter,
                         false,
                         current_max_eval,
                     );
@@ -182,24 +211,29 @@ where
                 IntegratedPhase::Imag => &f[1],
                 IntegratedPhase::Both => unimplemented!(),
             };
-            if let Err(err) = grid.add_training_sample(s, *sel_f) {
+            if let Err(err) = integration_state.grid.add_training_sample(s, *sel_f) {
                 warn!("WARNING: {}", err)
             } else {
-                integral.add_sample(*sel_f * s.get_weight(), Some(s));
+                integration_state
+                    .integral
+                    .add_sample(*sel_f * s.get_weight(), Some(s));
             }
         }
 
         let new_meta_data = StatisticsCounter::from_evaluation_results(&evaluation_results);
-        stats = stats.merged(&new_meta_data);
+        integration_state.stats = integration_state.stats.merged(&new_meta_data);
 
-        grid.update(settings.integrator.learning_rate);
-        integral.update_iter();
+        integration_state
+            .grid
+            .update(settings.integrator.learning_rate);
+        integration_state.integral.update_iter();
 
         for i_integrand in 0..N_INTEGRAND_ACCUMULATORS {
             for (s, f) in samples[..cur_points].iter().zip(&f_evals[..cur_points]) {
-                all_integrals[i_integrand].add_sample(f[i_integrand] * s.get_weight(), Some(s));
+                integration_state.all_integrals[i_integrand]
+                    .add_sample(f[i_integrand] * s.get_weight(), Some(s));
             }
-            if !all_integrals[i_integrand].update_iter() {
+            if !integration_state.all_integrals[i_integrand].update_iter() {
                 info!("WARNING: grid update failed, likely due to insufficient number of sample points to be considered.");
             }
         }
@@ -214,22 +248,26 @@ where
             let mut tabled_data = vec![];
 
             tabled_data.push(IntegralResult {
-                id: format!("Sum@it#{}", integral.cur_iter),
-                n_samples: format!("{}", integral.processed_samples),
+                id: format!("Sum@it#{}", integration_state.integral.cur_iter),
+                n_samples: format!("{}", integration_state.integral.processed_samples),
                 n_samples_perc: format!("{:.3e}%", 100.),
-                integral: format!("{:.8e}", integral.avg),
+                integral: format!("{:.8e}", integration_state.integral.avg),
                 variance: format!(
                     "{:.8e}",
-                    integral.err * ((integral.processed_samples - 1).max(0) as f64).sqrt()
+                    integration_state.integral.err
+                        * ((integration_state.integral.processed_samples - 1).max(0) as f64).sqrt()
                 ),
-                err: format!("{:.8e}", integral.err),
+                err: format!("{:.8e}", integration_state.integral.err),
                 err_perc: format!(
                     "{:.3e}%",
-                    (integral.err / (integral.avg.abs()).max(1.0e-99)).abs() * 100.
+                    (integration_state.integral.err
+                        / (integration_state.integral.avg.abs()).max(1.0e-99))
+                    .abs()
+                        * 100.
                 ),
                 pdf: String::from_str("N/A").unwrap(),
             });
-            if let Grid::Discrete(g) = &grid {
+            if let Grid::Discrete(g) = &integration_state.grid {
                 for (i, b) in g.bins.iter().enumerate() {
                     tabled_data.push(IntegralResult {
                         id: format!("chann#{}", i),
@@ -237,7 +275,7 @@ where
                         n_samples_perc: format!(
                             "{:.3e}%",
                             ((b.accumulator.processed_samples as f64)
-                                / (integral.processed_samples.max(1) as f64))
+                                / (integration_state.integral.processed_samples.max(1) as f64))
                                 * 100.
                         ),
                         integral: format!("{:.8e}", b.accumulator.avg),
@@ -257,14 +295,14 @@ where
                 }
             }
             let mut f = BufWriter::new(
-                File::create(&format!("results_it_{}.txt", iter))
+                File::create(&format!("results_it_{}.txt", integration_state.iter))
                     .expect("Could not create results file"),
             );
             writeln!(f, "{}", Table::new(tabled_data).with(Style::psql())).unwrap();
         }
 
-        iter += 1;
-        num_points += cur_points;
+        integration_state.iter += 1;
+        integration_state.num_points += cur_points;
 
         info!(
             "/  [ {} ] {}: n_pts={:-6.0}K {} {} /sample/core ",
@@ -273,20 +311,29 @@ where
                 utils::format_wdhms(t_start.elapsed().as_secs() as usize)
             )
             .bold(),
-            format!("Iteration #{:-4}", iter).bold().green(),
+            format!("Iteration #{:-4}", integration_state.iter)
+                .bold()
+                .green(),
             cur_points as f64 / 1000.,
-            if num_points >= 10_000_000 {
-                format!("n_tot={:-7.0}M", num_points as f64 / 1_000_000.)
-                    .bold()
-                    .green()
+            if integration_state.num_points >= 10_000_000 {
+                format!(
+                    "n_tot={:-7.0}M",
+                    integration_state.num_points as f64 / 1_000_000.
+                )
+                .bold()
+                .green()
             } else {
-                format!("n_tot={:-7.0}K", num_points as f64 / 1000.)
-                    .bold()
-                    .green()
+                format!(
+                    "n_tot={:-7.0}K",
+                    integration_state.num_points as f64 / 1000.
+                )
+                .bold()
+                .green()
             },
             format!(
                 "{:-17.3} ms",
-                (((t_start.elapsed().as_secs() as f64) * 1000.) / (num_points as f64))
+                (((t_start.elapsed().as_secs() as f64) * 1000.)
+                    / (integration_state.num_points as f64))
                     * (cores as f64)
             )
             .bold()
@@ -295,9 +342,9 @@ where
 
         for i_integrand in 0..(N_INTEGRAND_ACCUMULATORS / 2) {
             print_integral_result(
-                &all_integrals[2 * i_integrand],
+                &integration_state.all_integrals[2 * i_integrand],
                 i_integrand + 1,
-                iter,
+                integration_state.iter,
                 "re",
                 if i_integrand == 0 {
                     target.map(|o| o.re).or(None)
@@ -306,9 +353,9 @@ where
                 },
             );
             print_integral_result(
-                &all_integrals[2 * i_integrand + 1],
+                &integration_state.all_integrals[2 * i_integrand + 1],
                 i_integrand + 1,
-                iter,
+                integration_state.iter,
                 "im",
                 if i_integrand == 0 {
                     target.map(|o| o.im).or(None)
@@ -327,9 +374,11 @@ where
                 for part in 0..=1 {
                     for sgn in 0..=1 {
                         if (if sgn == 0 {
-                            all_integrals[2 * i_integrand + part].max_eval_positive
+                            integration_state.all_integrals[2 * i_integrand + part]
+                                .max_eval_positive
                         } else {
-                            all_integrals[2 * i_integrand + part].max_eval_negative
+                            integration_state.all_integrals[2 * i_integrand + part]
+                                .max_eval_negative
                         }) == 0.
                         {
                             continue;
@@ -346,17 +395,21 @@ where
                             format!(
                                 "{:+.16e}",
                                 if sgn == 0 {
-                                    all_integrals[2 * i_integrand + part].max_eval_positive
+                                    integration_state.all_integrals[2 * i_integrand + part]
+                                        .max_eval_positive
                                 } else {
-                                    all_integrals[2 * i_integrand + part].max_eval_negative
+                                    integration_state.all_integrals[2 * i_integrand + part]
+                                        .max_eval_negative
                                 }
                             ),
                             format!(
                                 "( {} )",
                                 if let Some(sample) = if sgn == 0 {
-                                    &all_integrals[2 * i_integrand + part].max_eval_positive_xs
+                                    &integration_state.all_integrals[2 * i_integrand + part]
+                                        .max_eval_positive_xs
                                 } else {
-                                    &all_integrals[2 * i_integrand + part].max_eval_negative_xs
+                                    &integration_state.all_integrals[2 * i_integrand + part]
+                                        .max_eval_negative_xs
                                 } {
                                     match sample {
                                         Sample::Continuous(_w, v) => v
@@ -376,27 +429,36 @@ where
             }
         }
 
-        stats.display_status();
+        integration_state.stats.display_status();
 
         // now merge all statistics and observables into the first
         let (first, others) = user_data.integrand[..cores].split_at_mut(1);
         for other_itg in others {
-            first[0].merge_results(other_itg, iter);
+            first[0].merge_results(other_itg, integration_state.iter);
         }
 
         // now write the observables to disk
         if let Some(itg) = user_data.integrand[..cores].first_mut() {
-            itg.update_results(iter);
+            itg.update_results(integration_state.iter);
         }
         info!("");
     }
 
     IntegrationResult {
-        neval: integral.processed_samples as i64,
-        fail: integral.num_zero_evaluations as i32,
-        result: all_integrals.iter().map(|res| res.avg).collect::<Vec<_>>(),
-        error: all_integrals.iter().map(|res| res.err).collect::<Vec<_>>(),
-        prob: all_integrals
+        neval: integration_state.integral.processed_samples as i64,
+        fail: integration_state.integral.num_zero_evaluations as i32,
+        result: integration_state
+            .all_integrals
+            .iter()
+            .map(|res| res.avg)
+            .collect::<Vec<_>>(),
+        error: integration_state
+            .all_integrals
+            .iter()
+            .map(|res| res.err)
+            .collect::<Vec<_>>(),
+        prob: integration_state
+            .all_integrals
             .iter()
             .map(|res| res.chi_sq)
             .collect::<Vec<_>>(),
