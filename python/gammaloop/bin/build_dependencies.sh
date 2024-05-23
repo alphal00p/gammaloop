ed2b1cf763dada5b90468ba455b6b54df6b6aae8
lcnbr <im@lcnbr.ch>
2024-05-23 10:08:22 +0200

    fix: :bug: added cd back in the maturin build part

--- python/gammaloop/bin/build_dependencies.sh
+++ python/gammaloop/bin/build_dependencies.sh
@@ -187,9 +187,11 @@ build_dependencies () {
         CMD_TO_ACCESS_SYMBOLICA="${CMD_TO_ACCESS_SYMBOLICA:-git clone https://github.com/alphal00p/symbolica}"
         $CMD_TO_ACCESS_SYMBOLICA >> dependency_build.log 2>&1
         cd symbolica && git reset --hard f6fc622 >> ../dependency_build.log 2>&1
+        cd ..
     fi
 
     if ! test -f symbolica/symbolica_path.txt; then
+        cd symbolica
 
         if [ "$1" == "with_venv" ]
         then
