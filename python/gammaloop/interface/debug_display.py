84bbbe7527b9f48f00207d73b139cbb2d118b488
Mathijs Fraaije <mathijsfraaije@gmail.com>
2024-08-21 16:49:35 +0200

    final result in display

--- python/gammaloop/interface/debug_display.py
+++ python/gammaloop/interface/debug_display.py
@@ -24,7 +24,7 @@ def build_debug_dict(log_file: str) -> Dict[str, Any]:
 # I assume one sample from the integrator per point
 def display_havana_sample(debug_dict: Dict[str, Any]) -> None:
     if 'havana_sample' not in debug_dict:
-        logger.info("no havana sample in debug info")
+        logger.warn("no havana sample in debug info")
         return
 
     sample = debug_dict['havana_sample'][0]
@@ -34,20 +34,42 @@ def display_havana_sample(debug_dict: Dict[str, Any]) -> None:
 
 def display_momenta_samples(debug_dict: Dict[str, Any]) -> None:
     if 'momenta_sample' not in debug_dict:
-        logger.info("no momenta sample in debug info")
+        logger.warn("no momenta sample in debug info")
+        return
 
     for gammaloop_sample in debug_dict['momenta_sample']:
         logger.info("momenta sampled: ")
         for index, loop_momentum in enumerate(gammaloop_sample['loop_moms']):
             logger.info("\t loop momentum {}: {}".format(index, loop_momentum))
 
+        logger.info("")
         for index, external_momentum in enumerate(gammaloop_sample['external_moms']):
             logger.info("\t external momentum {}: {}".format(
                 index, external_momentum))
 
+        logger.info("")
         logger.info("\t jacobian: {}".format(gammaloop_sample['jacobian']))
+        logger.info("")
+
+
+def display_final_result(debug_dict: Dict[str, Any]) -> None:
+    if 'final_result' not in debug_dict:
+        logger.warn("no final result in debug info")
+        return
+
+    res = debug_dict['final_result'][0]
+    res_real = res['re']
+    res_imag = res['im']
+
+    if res_imag >= 0:
+        format_result = "{} + {}i".format(res_real, res_imag)
+    else:
+        format_result = "{} - {}i".format(res_real, abs(res_imag))
+
+    logger.info("final result: {}".format(format_result))
 
 
 def display_default(debug_dict: Dict[str, Any]) -> None:
     display_havana_sample(debug_dict)
     display_momenta_samples(debug_dict)
+    display_final_result(debug_dict)
