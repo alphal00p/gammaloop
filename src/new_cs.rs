727b5e476a6209536958ec0717a0631766515345
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-01-28 17:18:29 +0100

    beginnings of pipeline test and conversion of generation output to hedge graph

--- src/new_cs.rs
+++ src/new_cs.rs
@@ -4,21 +4,25 @@ use ahash::HashMap;
 use bitvec::vec::BitVec;
 use color_eyre::Result;
 
+use itertools::Itertools;
 use linnet::half_edge::{
     subgraph::{cycle::SignedCycle, Inclusion, OrientedCut, SubGraph, SubGraphOps},
-    EdgeData, EdgeId, Hedge, HedgeGraph, HedgeVec, Involution, Orientation, Parent, TraversalTree,
+    EdgeData, EdgeId, Flow, Hedge, HedgeGraph, HedgeGraphBuilder, HedgeVec, Involution, NodeIndex,
+    Orientation, Parent, TraversalTree,
 };
 use symbolica::atom::{representation::InlineNum, Atom};
 
 use crate::{
     feyngen::{
-        diagram_generator::FeynGen, FeynGenOptions, GenerationType,
-        NumeratorAwareGraphGroupingOption,
+        diagram_generator::FeynGen, FeynGenFilter, FeynGenFilters, FeynGenOptions, GenerationType,
+        NumeratorAwareGraphGroupingOption, SelfEnergyFilterOptions, SnailFilterOptions,
+        TadpolesFilterOptions,
     },
-    graph::{BareGraph, DerivedGraphData, Edge, LoopMomentumBasis, Vertex},
+    graph::{BareGraph, DerivedGraphData, Edge, EdgeType, LoopMomentumBasis, Vertex},
     model::Model,
     momentum::{SignOrZero, Signature},
     numerator::{NumeratorState, PythonState, UnInit},
+    tests_from_pytest::load_generic_model,
     ProcessSettings,
 };
 
@@ -183,6 +187,35 @@ struct GenerationOptions {
     pub loop_momentum_bases: Option<HashMap<String, Vec<String>>>,
 }
 
+// should produce e+e- -> d d~ g
+fn test_process() -> GenerationOptions {
+    let feyngen_options = FeynGenOptions {
+        amplitude_filters: FeynGenFilters(vec![]),
+        cross_section_filters: FeynGenFilters(vec![
+            FeynGenFilter::SelfEnergyFilter(SelfEnergyFilterOptions::default()),
+            FeynGenFilter::TadpolesFilter(TadpolesFilterOptions::default()),
+            FeynGenFilter::ZeroSnailsFilter(SnailFilterOptions::default()),
+        ]),
+        initial_pdgs: vec![11, -11],
+        final_pdgs: vec![1, -1, 21],
+        generation_type: GenerationType::CrossSection,
+        symmetrize_final_states: false,
+        symmetrize_initial_states: false,
+        symmetrize_left_right_states: false,
+        loop_count_range: (2, 2),
+    };
+
+    GenerationOptions {
+        feyngen_options,
+        numerator_aware_isomorphism_grouping: NumeratorAwareGraphGroupingOption::OnlyDetectZeroes,
+        filter_self_loop: true,
+        graph_prefix: "GL".to_string(),
+        selected_graphs: None,
+        vetoed_graphs: None,
+        loop_momentum_bases: None,
+    }
+}
+
 impl Process {
     pub fn generate(options: GenerationOptions, model: &Model) -> Result<Self> {
         let definition = ProcessDefinition {
@@ -201,6 +234,51 @@ impl Process {
             options.vetoed_graphs,
             options.loop_momentum_bases,
         )?;
+
+        let hedge_collection = bare_collection
+            .into_iter()
+            .map(|bare_graph| {
+                let mut hedge_graph_builder = HedgeGraphBuilder::<Edge, Vertex>::new();
+
+                for vertex in bare_graph.vertices {
+                    // do we need to remove the vertices attached to externals?
+                    hedge_graph_builder.add_node(vertex.clone());
+                }
+
+                for edge in bare_graph.edges {
+                    match edge.edge_type {
+                        EdgeType::Virtual => {
+                            hedge_graph_builder.add_edge(
+                                NodeIndex(edge.vertices[0]),
+                                NodeIndex(edge.vertices[1]),
+                                edge,
+                                true,
+                            );
+                        }
+                        EdgeType::Incoming => {
+                            hedge_graph_builder.add_external_edge(
+                                NodeIndex(edge.vertices[1]),
+                                edge,
+                                // I am not sure what to put for these two
+                                true,
+                                Flow::Source,
+                            );
+                        }
+                        EdgeType::Outgoing => {
+                            hedge_graph_builder.add_external_edge(
+                                NodeIndex(edge.vertices[0]),
+                                edge,
+                                // I am not sure what to put for these two
+                                true,
+                                Flow::Source,
+                            );
+                        }
+                    }
+                }
+
+                hedge_graph_builder.build()
+            })
+            .collect_vec();
 
         let collection = match options.feyngen_options.generation_type {
             GenerationType::Amplitude => {
@@ -298,4 +376,13 @@ pub struct CrossSectionCut {
     cut: OrientedCut,
     left: BitVec,
     right: BitVec,
+}
+
+#[test]
+fn test_new_structure() {
+    let model = load_generic_model("sm");
+    let generation_options = test_process();
+
+    let mut process_list = ProcessList::new();
+    process_list.generate(generation_options, &model).unwrap();
 }
