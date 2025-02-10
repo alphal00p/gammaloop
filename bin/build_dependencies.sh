5c8e743215cadb85a872db2b0add4c0b2898ed62
lcnbr <im@lcnbr.ch>
2025-02-10 19:26:07 +0100

    point to symbolica main on our fork

--- bin/build_dependencies.sh
+++ bin/build_dependencies.sh
@@ -1,8 +1,9 @@
 cd ./python/gammaloop
 #export CMD_TO_ACCESS_SYMBOLICA="${CMD_TO_ACCESS_SYMBOLICA:-git clone https://github.com/benruijl/symbolica}"
 # export CMD_TO_ACCESS_SYMBOLICA="${CMD_TO_ACCESS_SYMBOLICA:-git clone -b main https://github.com/benruijl/symbolica}"
-export CMD_TO_ACCESS_SYMBOLICA="${CMD_TO_ACCESS_SYMBOLICA:-git clone -b no_namespace https://github.com/alphal00p/symbolica}"
+export CMD_TO_ACCESS_SYMBOLICA="${CMD_TO_ACCESS_SYMBOLICA:-git clone -b main https://github.com/alphal00p/symbolica}"
 export SYMBOLICA_BUILD_PROFILE="${SYMBOLICA_BUILD_PROFILE:-release}"
 #export SYMBOLICA_REVISION_HASH="${SYMBOLICA_REVISION_HASH:-latest}"
-export SYMBOLICA_REVISION_HASH="${SYMBOLICA_REVISION_HASH:-3f1a5d17abde15df49ca092f311fcd685c1d56fc}"
+export SYMBOLICA_REVISION_HASH="${SYMBOLICA_REVISION_HASH:-71608003a2ceeb09747c84975694ebcdc9ff44a3}"
+
 ./bin/build_dependencies.sh "$@"
---
+++ bin/patch_symbolica.py
@@ -0,0 +1,1 @@
+../../bin/patch_symbolica.py
