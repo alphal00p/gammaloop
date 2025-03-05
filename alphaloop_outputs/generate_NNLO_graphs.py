37110b26e5398b5910dc0cc7ed50535c4a2d03f2
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-03-05 13:41:34 +0100

    outer_nf in generate_NNLO_graphs.py

--- alphaloop_outputs/generate_NNLO_graphs.py
+++ alphaloop_outputs/generate_NNLO_graphs.py
@@ -33,6 +33,7 @@ exec(contents)
 print("nf_graphs=", nf_graphs)
 print("s_channel_singlet=", s_channel_singlet)
 print("t_channel_singlet=", t_channel_singlet)
+print("outer_nf_graphs"=outer_nf_graphs)
 print("no_valid_lmb=", no_valid_lmb)
 
 # Fixes to graph
@@ -50,6 +51,15 @@ print("no_valid_lmb=", no_valid_lmb)
 #        ]
 
 inputs = [
+    (
+        'NNLO_from_gL__outer_nf', [
+            (g_name, g) for (g_name, g) in graphs if (
+                g_name in outer_nf_graphs and
+                # this is already tested in rust, but we can do it again
+                g_name not in s_channel_singlet and
+                g_name not in t_channel_singlet)
+        ],
+    ),
     (
         'NNLO_from_gL__nf__not_singlet',
         [
@@ -94,14 +104,14 @@ for (category, selected_graphs) in inputs:
             print("Non-singlet diagrams should always have a valid LMB!")
             sys.exit(1)
 
-summed_selected_sg_name_list = sorted([sg_name for (
-    cat, selected_graphs) in inputs for (sg_name, _sg) in selected_graphs])
-sg_name_list = sorted([gn for (gn, g) in graphs])
-if summed_selected_sg_name_list != sg_name_list:
+    summed_selected_sg_name_list = sorted([sg_name for (
+        cat, selected_graphs) in inputs for (sg_name, _sg) in selected_graphs])
+    sg_name_list = sorted([gn for (gn, g) in graphs])
+    if summed_selected_sg_name_list != sg_name_list:
     print("Category selections do not add up to the original list of graphs!")
     sys.exit(1)
 
-for (category, selected_graphs) in inputs:
+    for (category, selected_graphs) in inputs:
     with open(f'{category}.py', 'w') as f:
         f.write("graphs={}\ngraph_names=[{}]".format(
             pformat([g for g_name, g in selected_graphs]),
