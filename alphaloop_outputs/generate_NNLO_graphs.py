b62633375fc0a1ceb96b78485d4bf8391921a9fb
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-03-05 13:59:28 +0100

    fix indentation in NNLO_graphs.py, push generated scripts

--- alphaloop_outputs/generate_NNLO_graphs.py
+++ alphaloop_outputs/generate_NNLO_graphs.py
@@ -33,7 +33,7 @@ exec(contents)
 print("nf_graphs=", nf_graphs)
 print("s_channel_singlet=", s_channel_singlet)
 print("t_channel_singlet=", t_channel_singlet)
-print("outer_nf_graphs"=outer_nf_graphs)
+print("outer_nf_graphs=", outer_nf_graphs)
 print("no_valid_lmb=", no_valid_lmb)
 
 # Fixes to graph
@@ -109,12 +109,12 @@ for (category, selected_graphs) in inputs:
         cat, selected_graphs) in inputs for (sg_name, _sg) in selected_graphs])
     sg_name_list = sorted([gn for (gn, g) in graphs])
     if summed_selected_sg_name_list != sg_name_list:
-    print("Category selections do not add up to the original list of graphs!")
-    sys.exit(1)
+        print("Category selections do not add up to the original list of graphs!")
+        sys.exit(1)
 
     for (category, selected_graphs) in inputs:
-    with open(f'{category}.py', 'w') as f:
-        f.write("graphs={}\ngraph_names=[{}]".format(
-            pformat([g for g_name, g in selected_graphs]),
-            ','.join('"%s"' % g_name for g_name, g in selected_graphs)
-        ))
+        with open(f'{category}.py', 'w') as f:
+            f.write("graphs={}\ngraph_names=[{}]".format(
+                pformat([g for g_name, g in selected_graphs]),
+                ','.join('"%s"' % g_name for g_name, g in selected_graphs)
+            ))
