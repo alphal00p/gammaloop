f2b187087998d86095e7d992d9265efa0d657a0c
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-03-05 17:26:18 +0100

    new structure for cff expressions with cuts

--- src/cff/cut_expression.rs
+++ src/cff/cut_expression.rs
@@ -1,12 +1,13 @@
 use derive_more::{From, Into};
 use linnet::half_edge::{hedgevec::HedgeVec, involution::Orientation};
 use serde::{Deserialize, Serialize};
+use typed_index_collections::TiVec;
 
 use crate::new_cs::CutId;
 
-use super::{surface::HybridSurfaceID, tree::Tree};
+use super::{generation::SurfaceCache, surface::HybridSurfaceID, tree::Tree};
 
-#[derive(Debug, Clone, Serialize, Deserialize, From, Into, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, From, Into, Hash, PartialEq, Eq, Copy)]
 pub struct OrientationID(usize);
 
 pub struct OrientationData {
@@ -16,7 +17,16 @@ pub struct OrientationData {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CutOrientationExpression {
-    pub orientation: OrientationID,
     pub left: Tree<HybridSurfaceID>,
     pub right: Tree<HybridSurfaceID>,
+}
+
+pub struct OrientationExpression {
+    pub data: OrientationData,
+    pub expressions: Vec<CutOrientationExpression>,
+}
+
+pub struct CFFCutExpression {
+    pub orientations: TiVec<OrientationID, OrientationExpression>,
+    pub surfaces: SurfaceCache,
 }
