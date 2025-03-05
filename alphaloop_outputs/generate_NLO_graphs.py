df20707c42fe538c5738e84618333558a5a9c6e9
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-03-05 11:47:02 +0100

    sort graphs

--- alphaloop_outputs/generate_NLO_graphs.py
+++ alphaloop_outputs/generate_NLO_graphs.py
@@ -1,6 +1,14 @@
 import yaml
 import os
 from pprint import pformat
+
+
+def sort_by_id(graph_name_and_graph):
+    graph_name, graph = graph_name_and_graph
+    strip_GL = graph_name.split('GL')[1]
+    return int(strip_GL)
+
+
 graphs = []
 
 category = 'NLO_graphs'
@@ -11,24 +19,27 @@ files = [os.path.join(category, file)
 
 for file in files:
     with open(file, 'r') as f:
-        graphs.append([os.path.basename(file)[:-5],yaml.load(f, Loader=yaml.FullLoader)])
+        graphs.append([os.path.basename(file)[:-5],
+                      yaml.load(f, Loader=yaml.FullLoader)])
+
+graphs.sort(key=sort_by_id)
 
 # Fixes to graph
-for (g_name, g) in graphs:
-    g['edges'] = {k+1: v for k,v in g['edges'].items()}
-    g['nodes'] = {k+1: v for k,v in g['nodes'].items()}
-    for e in g['edges'].values():
-        e['vertices'] = [v+1 for v in e['vertices']]
-    for i_n, n in g['nodes'].items():
-        n['edge_ids'] = [e+1 for e in n['edge_ids']]
-        n['indices'] = [
-                g['edges'][e_id]['indices'][0] if (len(g['edges'][e_id]['indices'])==1 or g['edges'][e_id]['vertices'][0] == i_n)
-                else g['edges'][e_id]['indices'][1]
-            for e_id in n['edge_ids']
-        ]
+# for (g_name, g) in graphs:
+#    g['edges'] = {k+1: v for k,v in g['edges'].items()}
+#    g['nodes'] = {k+1: v for k,v in g['nodes'].items()}
+#    for e in g['edges'].values():
+#        e['vertices'] = [v+1 for v in e['vertices']]
+#    for i_n, n in g['nodes'].items():
+#        n['edge_ids'] = [e+1 for e in n['edge_ids']]
+#        n['indices'] = [
+#                g['edges'][e_id]['indices'][0] if (len(g['edges'][e_id]['indices'])==1 or g['edges'][e_id]['vertices'][0] == i_n)
+#                else g['edges'][e_id]['indices'][1]
+#            for e_id in n['edge_ids']
+#        ]
 
 with open(f'NLO_from_gL.py', 'w') as f:
     f.write("graphs={}\ngraph_names=[{}]".format(
         pformat([g for g_name, g in graphs]),
-        ','.join('"%s"'%g_name for g_name, g in graphs)
+        ','.join('"%s"' % g_name for g_name, g in graphs)
     ))
