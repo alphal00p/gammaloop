aee3428b942e732248e7502dff42355ac93ffe99
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-04-28 20:17:57 +0200

    trait impl for amplitude integrand

--- src/new_gammaloop_integrand/amplitude_integrand.rs
+++ src/new_gammaloop_integrand/amplitude_integrand.rs
@@ -1,11 +1,84 @@
 use spenso::complex::Complex;
 use symbolica::numerical_integration::{Grid, Sample};
+use typed_index_collections::TiVec;
 
-use crate::{evaluation_result::EvaluationResult, integrands::HasIntegrand, utils::F, Settings};
+use crate::{
+    evaluation_result::EvaluationResult,
+    integrands::HasIntegrand,
+    momentum::{Polarization, Rotation},
+    momentum_sample::MomentumSample,
+    new_graph::{Graph, LmbIndex, LoopMomentumBasis},
+    utils::{FloatLike, F},
+    Polarizations, Settings,
+};
+
+use super::{GammaloopIntegrand, GraphTerm, LmbMultiChannelingSetup};
+
+#[derive(Clone)]
+pub struct AmplitudeGraphTerm {
+    graph: Graph,
+    multi_channeling_setup: LmbMultiChannelingSetup,
+    lmbs: TiVec<LmbIndex, LoopMomentumBasis>,
+}
+
+impl AmplitudeGraphTerm {
+    fn evaluate<T: FloatLike>(
+        &self,
+        momentum_sample: &MomentumSample<T>,
+        settings: &Settings,
+    ) -> Complex<F<T>> {
+        todo!()
+    }
+}
+
+impl GraphTerm for AmplitudeGraphTerm {
+    fn get_graph(&self) -> &Graph {
+        &self.graph
+    }
+
+    fn get_multi_channeling_setup(&self) -> &LmbMultiChannelingSetup {
+        &self.multi_channeling_setup
+    }
+
+    fn get_lmbs(&self) -> &TiVec<LmbIndex, LoopMomentumBasis> {
+        &self.lmbs
+    }
+
+    fn evaluate<T: FloatLike>(
+        &self,
+        momentum_sample: &MomentumSample<T>,
+        settings: &Settings,
+    ) -> Complex<F<T>> {
+        self.evaluate(momentum_sample, settings)
+    }
+}
 
 #[derive(Clone)]
 pub struct AmplitudeIntegrand {
     pub settings: Settings,
+    pub rotations: Vec<Rotation>,
+    pub polarizations: Vec<Polarizations>,
+    pub graph_terms: Vec<AmplitudeGraphTerm>,
+}
+
+impl GammaloopIntegrand for AmplitudeIntegrand {
+    type G = AmplitudeGraphTerm;
+
+    fn get_rotations(&self) -> impl Iterator<Item = &Rotation> {
+        self.rotations.iter()
+    }
+
+    fn get_terms(&self) -> impl Iterator<Item = &Self::G> {
+        self.graph_terms.iter()
+    }
+
+    fn get_settings(&self) -> &Settings {
+        &self.settings
+    }
+
+    fn get_graph(&self, graph_id: usize) -> &Self::G {
+        &self.graph_terms[graph_id]
+    }
 }
 
 impl HasIntegrand for AmplitudeIntegrand {
