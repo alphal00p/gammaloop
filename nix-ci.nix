23698223c9873fdefff3f639f7ed629b04d71286
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-04-29 14:46:36 +0200

    manually-specifie dependencies

--- nix-ci.nix
+++ nix-ci.nix
@@ -1,23 +1,40 @@
-{
-  systems = ["x86_64-linux"];
+let system = "x86_64-linux";
+in {
+  systems = [ system ];
   doNotBuild = [
-    "checks.x86_64-linux.gammaloop-doctest"
-    "checks.x86_64-linux.gammaloop-nextest"
+    "checks.${system}.gammaloop-doctest"
+    "checks.${system}.gammaloop-nextest"
   ];
   fail-fast = false;
+  # We specify dependencies manually
+  # See https://nix-ci.com/documentation/automatic-dependency-discovery
+  # and https://nix-ci.com/documentation/manually-specified-dependencies
+  dependency-discovery.enable = false;
+  dependencies = {
+    "packages.${system}.gammaloop" = [
+      "packages.${system}.cargoArtefacts"
+      "packages.${system}.gammaloop-fmt"
+      "devShells.${system}.default"
+    ];
+    "packages.${system}.gammaloop-clippy" = [ "packages.${system}.gammaloop" ];
+    "packages.${system}.gammaloop-doc" = [ "packages.${system}.gammaloop" ];
+    "packages.${system}.gammaloop-llvm-coverage" = [ "packages.${system}.gammaloop" ];
+    "packages.${system}.nix-ci-check-gammaloop-doctest" = [ "packages.${system}.gammaloop" ];
+    "packages.${system}.nix-ci-check-gammaloop-nextest" = [ "packages.${system}.gammaloop" ];
+  };
   test = {
     gammaloop-doctest = {
       package = "packages.x86_64-linux.nix-ci-check-gammaloop-doctest";
       system = "x86_64-linux";
       in-repo = true;
-      secrets = ["SYMBOLICA_LICENSE"];
+      secrets = [ "SYMBOLICA_LICENSE" ];
     };
 
     gammaloop-nextest = {
       package = "packages.x86_64-linux.nix-ci-check-gammaloop-nextest";
       system = "x86_64-linux";
       in-repo = true;
-      secrets = ["SYMBOLICA_LICENSE"];
+      secrets = [ "SYMBOLICA_LICENSE" ];
     };
   };
 }
