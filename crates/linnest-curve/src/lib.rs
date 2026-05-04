00c3a829e36309791172eed89fefbd7f134939b2
lcnbr <im@lcnbr.ch>
2026-05-04 13:09:51 +0200

    add curve path patterns

--- crates/linnest-curve/src/lib.rs
+++ crates/linnest-curve/src/lib.rs
@@ -1,8 +1,8 @@
 mod curve_api;
 
 pub use curve_api::{
-    curve_hobby_through_bytes, curve_split_cubic_bytes, curve_split_quadratic_through_bytes,
-    curve_trim_cubic_bytes,
+    curve_hobby_through_bytes, curve_pattern_cubic_bytes, curve_split_cubic_bytes,
+    curve_split_quadratic_through_bytes, curve_trim_cubic_bytes,
 };
 
 #[cfg(target_arch = "wasm32")]
@@ -33,4 +33,10 @@ pub fn curve_split_quadratic_through(arg: &[u8]) -> Result<Vec<u8>, String> {
 #[wasm_func]
 pub fn curve_hobby_through(arg: &[u8]) -> Result<Vec<u8>, String> {
     curve_hobby_through_bytes(arg)
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_func]
+pub fn curve_pattern_cubic(arg: &[u8]) -> Result<Vec<u8>, String> {
+    curve_pattern_cubic_bytes(arg)
 }
