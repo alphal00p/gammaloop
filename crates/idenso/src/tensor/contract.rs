f19f06e764e86e48222744ce88fb5f050919782e
lcnbr <im@lcnbr.ch>
2026-05-15 12:26:24 +0200

    use macros in schoonschip metric test

--- crates/idenso/src/tensor/contract.rs
+++ crates/idenso/src/tensor/contract.rs
@@ -3,19 +3,19 @@ pub mod test {
     use super::super::AbstractIndex;
     use crate::schoonschip::Schoonschip;
     use crate::test_support::test_initialize;
-    use symbolica::{parse, symbol};
+    use spenso::{g, mink, p};
+    use symbolica::symbol;
 
     #[test]
     fn schoonschip_simplifies_metrics() {
         test_initialize();
-        symbol!("metric_simplify_mu", "metric_simplify_nu"; tags = ["spenso::index"]);
-        let expr = parse!(
-            "spenso::g(spenso::mink(4,metric_simplify_mu),spenso::mink(4,metric_simplify_nu))*p(spenso::mink(4,metric_simplify_nu))"
-        );
+        let (mu, nu) =
+            symbol!("metric_simplify_mu", "metric_simplify_nu"; tags = ["spenso::index"]);
+        let expr = g!(mink!(4, mu.clone()), mink!(4, nu.clone())) * p!(mink!(4, nu.clone()));
 
         assert_eq!(
             expr.schoonschip_net::<AbstractIndex>(),
-            parse!("p(spenso::mink(4,metric_simplify_mu))")
+            p!(mink!(4, mu.clone()))
         );
     }
 }
