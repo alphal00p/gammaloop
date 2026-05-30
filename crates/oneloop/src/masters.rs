ee6e67418356e4e58d87fdeaf13ebec2daa16a15
Elijah Cavan <elijahcavan@swishanalytics.com>
2026-05-30 16:28:53 -0400

    A0/C0/D0 symbols

--- crates/oneloop/src/masters.rs
+++ crates/oneloop/src/masters.rs
@@ -14,11 +14,20 @@ pub enum MasterIntegral {
         m2_sq: Atom,
     },
     Triangle {
+        p1_sq: Atom,
+        p2_sq: Atom,
+        p12_sq: Atom,
         m1_sq: Atom,
         m2_sq: Atom,
         m3_sq: Atom,
     },
     Box {
+        p1_sq: Atom,
+        p2_sq: Atom,
+        p3_sq: Atom,
+        p4_sq: Atom,
+        s: Atom,
+        t: Atom,
         m1_sq: Atom,
         m2_sq: Atom,
         m3_sq: Atom,
@@ -46,8 +55,30 @@ impl MasterBasis for OneLoopMasters {
 
     fn symbol(&self, integral: &MasterIntegral) -> Atom {
         match integral {
+            MasterIntegral::Tadpole { m_sq } => function!(S.a0, m_sq),
             MasterIntegral::Bubble { p_sq, m1_sq, m2_sq } => function!(S.b0, p_sq, m1_sq, m2_sq),
-            _ => todo!("A0/C0/D0 symbolic forms land with their masters (M4)"),
+            MasterIntegral::Triangle {
+                p1_sq,
+                p2_sq,
+                p12_sq,
+                m1_sq,
+                m2_sq,
+                m3_sq,
+            } => function!(S.c0, p1_sq, p2_sq, p12_sq, m1_sq, m2_sq, m3_sq),
+            MasterIntegral::Box {
+                p1_sq,
+                p2_sq,
+                p3_sq,
+                p4_sq,
+                s,
+                t,
+                m1_sq,
+                m2_sq,
+                m3_sq,
+                m4_sq,
+            } => function!(
+                S.d0, p1_sq, p2_sq, p3_sq, p4_sq, s, t, m1_sq, m2_sq, m3_sq, m4_sq
+            ),
         }
     }
 }
@@ -79,5 +110,66 @@ mod tests {
         let got = OneLoopMasters.symbol(&massless_bubble());
         let want = function!(S.b0, Atom::var(S.psq), Atom::Zero, Atom::Zero);
         assert_eq!(got, want);
+    }
+
+    #[test]
+    fn tadpole_maps_to_a0() {
+        crate::ensure_symbolica_license();
+        let m = MasterIntegral::Tadpole { m_sq: Atom::num(1) };
+        assert_eq!(OneLoopMasters.symbol(&m), function!(S.a0, Atom::num(1)));
+    }
+
+    #[test]
+    fn triangle_maps_to_c0() {
+        crate::ensure_symbolica_license();
+        let m = MasterIntegral::Triangle {
+            p1_sq: Atom::num(1),
+            p2_sq: Atom::num(2),
+            p12_sq: Atom::num(3),
+            m1_sq: Atom::Zero,
+            m2_sq: Atom::Zero,
+            m3_sq: Atom::Zero,
+        };
+        let want = function!(
+            S.c0,
+            Atom::num(1),
+            Atom::num(2),
+            Atom::num(3),
+            Atom::Zero,
+            Atom::Zero,
+            Atom::Zero
+        );
+        assert_eq!(OneLoopMasters.symbol(&m), want);
+    }
+
+    #[test]
+    fn box_maps_to_d0() {
+        crate::ensure_symbolica_license();
+        let m = MasterIntegral::Box {
+            p1_sq: Atom::Zero,
+            p2_sq: Atom::Zero,
+            p3_sq: Atom::Zero,
+            p4_sq: Atom::Zero,
+            s: Atom::num(1),
+            t: Atom::num(2),
+            m1_sq: Atom::Zero,
+            m2_sq: Atom::Zero,
+            m3_sq: Atom::Zero,
+            m4_sq: Atom::Zero,
+        };
+        let want = function!(
+            S.d0,
+            Atom::Zero,
+            Atom::Zero,
+            Atom::Zero,
+            Atom::Zero,
+            Atom::num(1),
+            Atom::num(2),
+            Atom::Zero,
+            Atom::Zero,
+            Atom::Zero,
+            Atom::Zero
+        );
+        assert_eq!(OneLoopMasters.symbol(&m), want);
     }
 }
--- crates/oneloop/src/symbols.rs
+++ crates/oneloop/src/symbols.rs
@@ -4,11 +4,17 @@ use symbolica::{atom::Symbol, symbol};
 pub struct OneLoopSymbols {
     pub ep: Symbol,
     pub psq: Symbol,
+    pub a0: Symbol,
     pub b0: Symbol,
+    pub c0: Symbol,
+    pub d0: Symbol,
 }
 
 pub static S: LazyLock<OneLoopSymbols> = LazyLock::new(|| OneLoopSymbols {
     ep: symbol!("oneloop::ep"),
     psq: symbol!("oneloop::psq"),
+    a0: symbol!("oneloop::A0"),
     b0: symbol!("oneloop::B0"),
+    c0: symbol!("oneloop::C0"),
+    d0: symbol!("oneloop::D0"),
 });
